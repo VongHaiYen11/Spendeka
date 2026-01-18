feb294ea1bedd53c7700021e6ef78f06d071cf7a
Vong Hai Yen <yenvonghailhbs6b24@gmail.com>
2026-01-18 13:17:01 +0700

    Add nativewind to the project

--- src/screens/summary/index.tsx
+++ src/screens/summary/index.tsx
@@ -1,10 +1,16 @@
 import { StyleSheet } from 'react-native';
 import { Text, View, SafeView } from '@/components/Themed';
+import { Text as RNText } from 'react-native';
 
 export default function Summary() { 
   return (
     <SafeView>
-      <Text>Summary</Text>
+      <Text>
+        Welcome to Nativewind!
+      </Text>
+      <RNText className="text-red-500 text-lg">
+        This is a normal React Native Text
+      </RNText>
     </SafeView>
   );
 }
